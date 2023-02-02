99f56b39e93b1ed1c0b665577635f45de3105b15
pangyu <1510397456@qq.com>
2023-02-02 18:58:37 +0800

    feat: 删除现场侧

--- src/views/site_side/index.vue
+++ src/views/site_side/index.vue
@@ -22,32 +22,42 @@
             :maxSize="20"
             :maxNumber="1"
             :api="upload"
-            @click="updateSiteId(site.id)"
+            @click="updateSiteId"
           />
-          <AButton type="danger" class="ml-4" @click="()=>{
-            updateSiteId(site.id)
-            openModal1()
-          }"> 删除现场侧 </AButton>
+          <AButton
+            type="danger"
+            class="ml-4"
+            @click="
+              () => {
+                updateSiteId(site.id)
+                showDeleteConfirm()
+              }
+            "
+          >
+            删除现场侧
+          </AButton>
         </span>
       </CardGrid>
     </Card>
-    {{ siteId }}
     <Modal1 @register="register1" :sideId="siteId" />
     <Modal4 @register="register4" />
   </PageWrapper>
 </template>
 <script lang="ts">
+  import Test from './Test.vue'
   import Modal1 from './Modal1.vue'
   import { BasicUpload } from '/@/components/Upload'
-  import { Card, CardGrid, notification } from 'ant-design-vue'
-  import { defineComponent, ref, nextTick, onMounted } from 'vue'
+  import {Card, CardGrid, Modal, notification} from 'ant-design-vue'
+  import {defineComponent, ref, nextTick, onMounted, createVNode} from 'vue'
   import { Alert, Space } from 'ant-design-vue'
   import { useModal } from '/@/components/Modal'
   import Modal4 from './Modal4.vue'
   import { PageWrapper } from '/@/components/Page'
   import { createImgPreview, ImagePreview } from '/@/components/Preview/index'
-  import { getSiteApi, videoApi } from '/@/api/sys/site'
+  import {deleteSiteApi, getSiteApi, videoApi} from '/@/api/sys/site'
   import { t } from '/@/hooks/web/useI18n'
+  import {ExclamationCircleOutlined} from "@ant-design/icons-vue";
+  import {useTabs} from "/@/hooks/web/useTabs";
   // import { PreviewActions } from '/@/components/Preview/src/typing';
 
   let siteId = ref(0)
@@ -74,6 +84,7 @@
   const imgList: string[] = ['https://picsum.photos/id/66/346/216']
   export default defineComponent({
     components: {
+      Test,
       Modal1,
       BasicUpload,
       Card,
@@ -89,6 +100,7 @@
       const [register1, { openModal: openModal1 }] = useModal()
       const modalVisible = ref<Boolean>(false)
       const userData = ref<any>(null)
+      const { refreshPage } = useTabs()
 
       const updateSiteId = (id: number) => {
         siteId.value = id
@@ -101,6 +113,38 @@
         }
         // 可以使用createImgPreview返回的 PreviewActions 来控制预览逻辑，实现类似幻灯片、自动旋转之类的骚操作
         createImgPreview({ imageList: imgList, defaultWidth: 700, rememberState: true, onImgLoad })
+      }
+
+      const deleteSite = async () => {
+        try {
+          const res = await deleteSiteApi({
+            siteId: siteId.value,
+          })
+          if (res) {
+            notification.success({
+              message: t('sys.site.siteSuccessTitle'),
+              duration: 2,
+            })
+          }
+        } catch (error) {}
+      }
+
+      const showDeleteConfirm = () => {
+        Modal.confirm({
+          title: '是否确认删除当前现场侧' + siteId.value,
+          icon: createVNode(ExclamationCircleOutlined),
+          okText: '是',
+          okType: 'danger',
+          cancelText: '否',
+          onOk() {
+            console.log('OK')
+            deleteSite()
+            refreshPage()
+          },
+          onCancel() {
+            console.log('Cancel')
+          },
+        })
       }
 
       let sites = ref<any>(null)
@@ -134,8 +178,10 @@
         userData,
         sites,
         updateSiteId,
+        showDeleteConfirm,
+        deleteSite,
+        refreshPage,
       }
     },
   })
-
 </script>
